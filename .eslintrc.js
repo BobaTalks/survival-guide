eaae594a78374d73a03c990c357cdbb54b4ebbee
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-02-21 19:49:11 -0800

    chore: use bt eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "react-app",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  ignorePatterns: ["node_modules/", "src/Images/", "build/", "public/"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
