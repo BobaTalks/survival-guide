62fdcf0f9ee49c2ef359b0a30acf297998cf0720
Maggie Chen <mchen@macnetwork.local>
2023-07-22 03:37:13 -0500

    finished splash page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import background_img from "./img/update.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="splash-page">
+      <div className="background">
+        <div className="splash-title">
+          <h2>Navigating High School and Beyond</h2>
+          <p>
+            We&apos;ve compiled the best advice from experts and seasoned
+            students alike to give you the tools you need to{" "}
+            <span>succeed</span>
+          </p>
+          <button className="button-begin">Let&apos;s get started</button>
+        </div>
+      </div>
     </div>
   );
 }
