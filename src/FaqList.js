e38b2da519f51ea60c0eca15f20e43201f1eca4e
Aayusha <aayyushashrestha@gmail.com>
2023-08-17 12:23:10 +0545

    added faq-lists

---
+++ src/FaqList.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+
+// const FaqList = (props) => {
+//     const faqs = props.faqs;
+//     return (
+//         <div className="faq-lists">
+//             {faqs.map((faq) => (
+//                 <div className ="faq-preview" key = {faq.id}>
+//                     <h2>{faq.title}</h2>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
+ 
+// export default FaqList;
