d65341cf5e93e9fc7f2a4db9e3389b571f92beda
oo-ng <122175989+oo-ng@users.noreply.github.com>
2023-08-30 19:47:26 -0400

    Updated all errors

--- src/MainBox.js
+++ src/MainBox.js
@@ -1,10 +1,7 @@
-// eslint-disable-next-line prettier/prettier
 import React from "react";
 import PropTypes from "prop-types";
 
 export const MainBox = ({ children }) => {
-  // eslint-disable-next-line prettier/prettier
-
   return (
     <div className="recipe recipe_scroll">
       <div>{children}</div>
