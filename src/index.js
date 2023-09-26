455300f3e6894a44171d88fee65e5ee48aa3429c
oo-ng <122175989+oo-ng@users.noreply.github.com>
2023-09-25 22:34:18 -0400

    fixing prettier problems

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
->>>>>>> e74854a797f231cb901e550dcceb00da22cce0cf
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
