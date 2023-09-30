1dd21ec7a61f03256ec35f5e661e06eb5e6c2f7b
oo-ng <122175989+oo-ng@users.noreply.github.com>
2023-09-30 14:37:02 -0400

    AP/IB page (#38)
    * completed required tasks
    
    * Updated all errors
    
    * Corrected casing errors
    
    * fixing prettier problems
    
    * fixing prettier problems
    
    * seperating pages for internship and APIB
    
    * ready to merge

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 export const MainBox = ({ children }) => {
   return (
-    <div className="recipe recipe_scroll">
-      <div className="secondaryContainer">{children}</div>
+    <div className="recipe recipe_scroll ">
+      <div>{children}</div>
     </div>
   );
 };
