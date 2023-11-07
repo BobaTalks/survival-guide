0c87aea001183c08a82e6260af5f423d01685749
maggie chen <sc2438@cornell.edu>
2023-11-06 21:25:52 -0500

    deleted stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import MainMenu from "./components/mainMenu/main_menu";
+import Menu from "./pages/menu";
 import Splash from "./pages/splash";
-
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Splash />}></Route>
-      <Route path="menu" element={<MainMenu />}></Route>
+      <Route path="menu" element={<Menu />}></Route>
+      <Route path="about" element={<About />}></Route>
       {/* <Route path="*" element={<PageNotFound />} /> */}
     </Routes>
   );
