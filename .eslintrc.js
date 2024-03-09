566e201b4f9fad808c6c71d9dddd596f5e134769
maggie chen <sc2438@cornell.edu>
2024-03-09 11:51:38 -0500

    synced up with bobatalks
    need to do prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'react-app', 'plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
-  ignorePatterns: ['node_modules/', 'src/Images/', 'build/', 'public/'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-hooks', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    'react/react-in-jsx-scope': 'off',
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Menu from "./pages/menu";
-import Splash from "./pages/splash";
+// import Menu from ;
+import Splash from "./pages/splash/splash";
 import Faq from './pages/faq/Faq';
 import { Routes, Route } from "react-router-dom";
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<Splash />}></Route>
-      <Route path="menu" element={<Menu />}></Route>
+      {/* <Route path="menu" element={<Menu />}></Route> */}
       <Route path="about" element={<About />}></Route>
       <Route path="faq" element={<Faq />}></Route>
       {/* <Route path="*" element={<PageNotFound />} /> */}
